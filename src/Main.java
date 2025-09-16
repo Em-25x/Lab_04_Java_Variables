108d2592740984d3229ce3b3cea085bcab769bc2
Em-25x <emmathehunter2007@gmail.com>
2025-09-15 22:36:29 -0400

    Inputed all arithmetics and did int sum

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,31 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+        int intOperandA = 5;
+        int intOperandB = 2;
+        int intSum = 3;
+        int intProduct = 4;
+        int intDifference = 1;
+        int intQuotient = 6;
+        int intModulo = 7;
+        //doubleOperandA, doubleOperandB, doubleSum, doubleProduct, doubleDifference, doubleQuotient
+        double doubleOperandA = 5.0;
+        double doubleOperandB = 2.0;
+        double doubleSum = 3.0;
+        double doubleProduct = 4.0;
+        double doubleDifference = 1.0;
+        double doubleQuotient = 6.0;
 
+        intSum = intOperandA + intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+        intProduct = intOperandA * intOperandB;
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
     }
 }
